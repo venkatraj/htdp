b54e1228550e8eb7feb55dc96e252acba3c28826
Venkat Raj <venkat@codinggeek.com>
2019-07-09 12:29:53 +0530

    1.5 exercise

---
+++ 1.5.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1.5|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(overlay/xy (ellipse 200 100 "solid" "white")
+               0 50
+           (ellipse 200 100 "solid" "red"))
