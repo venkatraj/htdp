90f6aeee7988a99d3cc4214f1ad069688fa385c8
Venkat Raj <venkat@codinggeek.com>
2019-07-11 12:00:25 +0530

    1.37 exercise

---
+++ 1.37.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1.37|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; String -> String
+; Removes first character from given string str and returns the rest
+(check-expect (string-rest "Venkat") "enkat")
+(check-expect (string-rest "Python") "ython")
+(check-error (string-rest "") "Empty string given")
+(define (string-rest str)
+  (if (string=? str "")
+      (error "Empty string given")
+      (substring str 1)))
