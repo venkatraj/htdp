7be08a5b58349568f65097ce7fcb64a56ebf17fa
Venkat Raj <venkat@codinggeek.com>
2019-07-11 11:48:07 +0530

    1.35 exercise

---
+++ 1.35.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1.35|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; String -> String
+; Extracts last character from given string str
+(check-expect (string-last "Venkat") "t")
+(check-expect (string-last "Python") "n")
+(check-expect (string-last "") "")
+(define (string-last str)
+  (if (string=? str "")
+      ""
+      (string-ith str (- (string-length str) 1))))
